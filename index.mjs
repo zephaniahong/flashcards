c7c91a4690c30bff3ec48512762563dda6ea6c6f
awongh <akira@awongh.com>
2021-01-08 17:03:07 +0800

    fix deps and setup

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './config/webpack.dev.js';
+import config from './webpack_conf/webpack.dev.js';
 
 // express imports
 import express from 'express';
