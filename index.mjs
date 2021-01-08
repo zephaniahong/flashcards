737391f016c32d23e5eb3768e9c7514244896f18
Akira Wong <awongh@gmail.com>
2021-01-08 16:34:41 +0800

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,51 @@
+// dev imports
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './config/webpack.dev.js';
+
+// express imports
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import methodOverride from 'method-override';
+
+import routes from './routes.mjs';
+
+const app = express();
+
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use(express.static('dist'));
+
+app.use(methodOverride('_method'));
+
+
+const env = process.env.NODE_ENV || 'development';
+
+if( env === 'development' ){
+  const compiler = webpack(config)
+
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    // html only
+    writeToDisk: filePath => /\.html$/.test(filePath),
+  }));
+  app.use(webpackHotMiddleware(compiler, {
+    log: false,
+    path: `/__webpack_hmr`,
+    heartbeat: 10 * 1000,
+  }));
+}
+
+// set the routes
+routes(app);
+
+const PORT = process.env.PORT || 3004;
+
+app.listen(PORT);
