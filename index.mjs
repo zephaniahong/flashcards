5dcd7113b851821eb8ac23c4919982e0da5c2580
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-15 16:30:28 +0800

    add comments and lint express index.mjs

--- index.mjs
+++ index.mjs
@@ -1,51 +1,49 @@
-// dev imports
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import methodOverride from 'method-override';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './webpack_conf/webpack.dev.js';
 
-// express imports
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import methodOverride from 'method-override';
+import webpackConfig from './webpack_conf/webpack.dev.js';
+import bindRoutes from './routes.mjs';
 
-import routes from './routes.mjs';
-
+// Initialise Express instance
 const app = express();
-
-app.use(cookieParser());
-
+// Set the Express view engine to expect EJS templates
 app.set('view engine', 'ejs');
-
+// Bind cookie parser middleware to parse cookies in requests
+app.use(cookieParser());
+// Bind Express middleware to parse request bodies for POST requests
 app.use(express.urlencoded({ extended: false }));
+// Bind Express middleware to parse JSON request bodies
 app.use(express.json());
-
+// Bind method override middleware to parse PUT and DELETE requests sent as POST requests
+app.use(methodOverride('_method'));
+// Expose the files stored in the public folder
 app.use(express.static('public'));
+// Expose the files stored in the distribution folder
 app.use(express.static('dist'));
 
-app.use(methodOverride('_method'));
-
-
+// Set up Webpack in dev env
 const env = process.env.NODE_ENV || 'development';
-
-if( env === 'development' ){
-  const compiler = webpack(config)
-
+if (env === 'development') {
+  const compiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
+    publicPath: webpackConfig.output.publicPath,
     // html only
-    writeToDisk: filePath => /\.html$/.test(filePath),
+    writeToDisk: (filePath) => /\.html$/.test(filePath),
   }));
   app.use(webpackHotMiddleware(compiler, {
     log: false,
-    path: `/__webpack_hmr`,
+    path: '/__webpack_hmr',
     heartbeat: 10 * 1000,
   }));
 }
 
-// set the routes
-routes(app);
+// Bind route definitions to the Express application
+bindRoutes(app);
 
+// Set Express to listen on the given port
 const PORT = process.env.PORT || 3004;
-
 app.listen(PORT);
