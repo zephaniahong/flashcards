d48e009c77e891c246b180d6bb71773ce0eddf9a
awongh <akira@awongh.com>
2021-02-07 16:40:42 +0800

    add comments to dynamic index.mjs import

--- index.mjs
+++ index.mjs
@@ -23,11 +23,17 @@ app.use(express.static('dist'));
 // Set up Webpack in dev env
 const env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
-  const { default: webpack } = await import('webpack')
 
+  // conditionally / dynamically import the webpack libraries we need in the dev environment
+  // a dynamic import syntax is called as a function and returns a promise
+  // use await so that every line is executed in order
+  // see more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#dynamic_import
+
+  // destructure the default import and name the variable
+  // see more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#importing_defaults
+  const { default: webpack } = await import('webpack')
   const { default: webpackDevMiddleware } =  await import('webpack-dev-middleware');
   const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
-
   const { default: webpackConfig } = await import('./webpack_conf/webpack.dev.js');
 
   const compiler = webpack(webpackConfig);
