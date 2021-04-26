97c714145960d95d162ab98911ec27c0039ec3ba
Zephaniah <zealorez@gmail.com>
2021-04-26 19:11:03 +0800

    configured migration file

---
+++ migrations/20210426105540-create-flashcards-tables.js
@@ -0,0 +1,159 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question: {
+        type: Sequelize.TEXT,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('decks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.TEXT,
+      },
+      card_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'cards',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      deck_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'decks',
+          key: 'id',
+        },
+      },
+      mastery: {
+        type: Sequelize.INTEGER,
+      },
+      time: {
+        type: Sequelize.TIME,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.TEXT,
+      },
+      email: {
+        type: Sequelize.TEXT,
+      },
+      password: {
+        type: Sequelize.TEXT,
+      },
+      session_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sessions',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+
+    });
+
+    await queryInterface.createTable('session_cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      card_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'cards',
+          key: 'id',
+        },
+      },
+      session_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sessions',
+          key: 'id',
+        },
+      },
+      familiarity: {
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('decks');
+    await queryInterface.dropTable('sessions');
+    await queryInterface.dropTable('session_cards');
+    await queryInterface.dropTable('cards');
+  },
+};
