3c60cbaf0fcdf7549e4b1665c83aec7e8556be14
Zephaniah <zealorez@gmail.com>
2021-04-26 18:53:44 +0800

    configured config file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'zephaniahong',
     password: null,
-    database: '<YOUR_APP_NAME>_development',
+    database: 'flashcards_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
