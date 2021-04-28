1fc0539d87e6f39ebd1836bf75f64aff29e2ac79
Zephaniah <zealorez@gmail.com>
2021-04-28 12:51:44 +0800

    conditionally render StudyDecks base on studyState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import StudyDecks from "./components/StudyDecks.jsx";
-
+import FlashCard from "./components/FlashCard.jsx";
 export default function App() {
+  const [studyState, setStudyState] = useState("");
+
+  // function to update study state
+  const updateState = (newState) => {
+    setStudyState(newState);
+  };
   return (
     <div>
-      <StudyDecks />
+      <StudyDecks updateState={updateState} studyState={studyState} />
     </div>
   );
 }
--- src/components/StudyDecks.jsx
+++ src/components/StudyDecks.jsx
@@ -1,14 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const StudyDecks = () => {
+const StudyDecks = ({ updateState, studyState }) => {
   const [decks, setDecks] = useState([]);
 
-  //create new session
+  // create new session
   const createSession = (deckId) => {
-    axios.post(`/createSession/${deckId}`).then((response) => {
-      console.log(response);
-    });
+    axios.post(`/createSession/${deckId}`);
+    updateState("study");
   };
 
   // get deck data only once OR whenever there is a change
@@ -20,20 +19,24 @@ const StudyDecks = () => {
   }, []);
 
   // create all the decks in card form
-  const deckList = decks.map((deck) => (
-    <div className="card" style={{ width: "12rem" }}>
-      <div className="card-body">
-        <h5 className="card-title">{deck.name}</h5>
-        <p className="card-text">Some quick example text</p>
-        <button
-          onClick={() => createSession(deck.id)}
-          className="btn btn-primary"
-        >
-          Study
-        </button>
+  let deckList;
+  if (studyState === "") {
+    deckList = decks.map((deck) => (
+      <div className="card" style={{ width: "12rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">{deck.name}</h5>
+          <p className="card-text">Some quick example text</p>
+          <button
+            type="button"
+            onClick={() => createSession(deck.id)}
+            className="btn btn-primary"
+          >
+            Study
+          </button>
+        </div>
       </div>
-    </div>
-  ));
+    ));
+  }
 
   // check if deck data has been received
   if (decks.length > 1) {
