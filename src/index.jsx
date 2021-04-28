8c70cdaf22f526a81d980cfa7561621b6864bd80
Zephaniah <zealorez@gmail.com>
2021-04-28 09:20:17 +0800

    add container class to root element

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './styles.scss';
+import React from "react";
+import { render } from "react-dom";
+import "./styles.scss";
 
-import App from './App.jsx';
+import App from "./App.jsx";
 
 // create an element that React will render stuff into
-const rootElement = document.createElement('div');
+const rootElement = document.createElement("div");
+rootElement.classList.add("container");
 
 // put that element onto the page
 document.body.appendChild(rootElement);
