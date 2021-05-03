61aea483ac6edb8eac166399f13027f40046d860
Zephaniah <zealorez@gmail.com>
2021-05-03 17:19:29 +0800

    add circular progress bar on dashboard

--- src/components/Mastery.jsx
+++ src/components/Mastery.jsx
@@ -1,7 +1,38 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const Mastery = () => {
-  return <div>Test</div>;
+const Mastery = ({ clickedDeck }) => {
+  // get all sessions for a single deck
+  if (clickedDeck !== null) {
+    axios.get(`/allSessions/${clickedDeck}`).then((response) => {
+      const mastery = response.data;
+      // get length of deck
+      axios.get(`/deckLength/${clickedDeck}`).then((response) => {
+        console.log(response.data);
+      });
+    });
+  }
+
+  return (
+    <div className="contain">
+      <div className="car">
+        <div className="box">
+          <div className="percent">
+            <svg>
+              <circle cx="70" cy="70" r="70"></circle>
+              <circle cx="70" cy="70" r="70"></circle>
+            </svg>
+            <div className="number">
+              <h2>
+                90<span>%</span>
+              </h2>
+            </div>
+          </div>
+          <h2 className="text">Mastery</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mastery;
