aa9f5760a630097c0a9e78cdd1d36034649ecac3
Zephaniah <zealorez@gmail.com>
2021-05-17 12:06:33 +0800

    fix flashcard not appearing

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -3,13 +3,11 @@ import React, { useState, useEffect } from "react";
 import FamiliarityButtons from "./FamiliarityButtons.jsx";
 import PriorityQueue from "../priorityQueue";
 const FlashCard = ({
-  selectedDeck,
+  clickedDeck,
   session,
   updateScoreState,
-  numCards,
   setCardCounter,
   cardCounter,
-  clickedDeck,
 }) => {
   const [deck, setDeck] = useState(null);
   const [cardState, setCardState] = useState("question");
@@ -22,15 +20,15 @@ const FlashCard = ({
   // set deck state after user chooses deck
   useEffect(() => {
     const srDeck = new PriorityQueue();
-    if (selectedDeck !== 0) {
-      axios.get(`/deck/${selectedDeck}`).then((response) => {
+    if (clickedDeck !== 0) {
+      axios.get(`/deck/${clickedDeck}`).then((response) => {
         const dbDeck = response.data.cards;
         let newDeck = [];
         for (let i = 0; i < dbDeck.length; i += 1) {
           newDeck.push([dbDeck[i], 0]);
         }
         // get all session_cards of a deck by a user
-        axios.get(`/allSessionCards/${selectedDeck}`).then((response2) => {
+        axios.get(`/allSessionCards/${clickedDeck}`).then((response2) => {
           const allSessionCards = response2.data;
           for (let i = 0; i < newDeck.length; i += 1) {
             for (let j = 0; j < allSessionCards.length; j += 1) {
